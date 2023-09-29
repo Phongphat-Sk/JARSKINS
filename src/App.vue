1b75d7cade13e8c5923b3a620f437a4a152ee839
Pencil05 <poowadach05@gmail.com>
2023-09-29 12:44:33 +0700

    ตกแต่ง

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,39 @@
       <div class="header-content">
         <!-- ด้านซ้ายมีข้อความว่า JARJOSUDLOR เป็น logo -->
         <div class="logo">JARJOSUDLOR</div>
-        <!-- ช่องค้นหาแบบวงลี -->
-        <div class="search-box">
-          <input class="search-input" type="text" placeholder="ค้นหา..." />
-        </div>
+        
         <!-- ทางขวาของช่องสีขาวมีลิงค์ตระกล้าสินค้า -->
         <div class="product-icon">
-         <a href="/">
-            <img src="/src/assets/pic/product-icon.png" alt="ตระกล้าสินค้า" />
-         </a>
-       </div>
-
+          <a href="/">
+            <img src="/src/assets/pic/product-icon.png" alt="ตระกล้าสินค้า"/>
+          </a>
+        </div>
+        
       </div>
-      <!-- สิ้นสุดส่วนเพิ่มเติม -->
     </header>
+
+    <div class="subhead">
+      <div class="alltag">
+        <div class="tags">
+          <button><a>Lucky Card</a></button>
+        </div>
+        <div class="tags">
+        <button><a>Lucky CardPlus</a></button>
+      </div>
+      <div class="tags">
+        <button><a>Give away</a></button>
+      </div>
+      <div class="tags">
+        <button><a>Market</a></button>
+      </div>
+      </div>
+
+      <div class="search-box">
+        <input class="search-input" type="text" placeholder="คุณกำลังมองหาอะไรอยู่"/>
+        <button class="serch-button" >ค้นหา</button>
+      </div>
+      <button class="login">เข้าสู่ระบบ</button>
+  </div>
     
     <!-- เนื้อหาแอปของคุณ -->
     <RouterView />
