a372aa4b13f444fa2de8c77c1e3d56021122771c
Pencil05 <poowadach05@gmail.com>
2023-10-02 12:45:13 +0700

    create home

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,100 @@
       </div>
       <button class="login">เข้าสู่ระบบ</button>
   </div>
-    
+  
+  <div class="nav">
+    <figure>  
+      <img src="src\assets\pic\chevron-arrow-up.png" alt="" width="25px" height="25px">
+      <img src="src\assets\pic\clipart1589423.png" alt="" width="25px" height="25px">
+      <img src="src\assets\pic\playerLeft.png" alt="" width="25px" height="25px">
+    </figure>
+  </div>
+  <div class="contai1">
+    <p>ค้นหาบ่อย : </p>
+    <div class="alltag">
+      <div class="tags">
+        <button><a>ค้นหาบ่อย</a></button>
+      </div>
+      <div class="tags">
+        <button><a>ค้นหาบ่อย</a></button>
+      </div>
+      <div class="tags">
+        <button><a>ค้นหาบ่อย</a></button>
+      </div>
+      <div class="tags">
+        <button><a>ค้นหาบ่อย</a></button>
+      </div>
+    </div>
+  </div>
+
+  <div class="contai2">
+    <figure>
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+    </figure>
+    <figure>
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+    </figure>
+  </div>
+
+  <div class="contai3">
+    <h2>Explore millions of offerings tailored to your business needs</h2>
+    <div class="bigfig">
+      <figure>
+        <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+    </figure>
+    <figure>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+    </figure>
+    </div>
+  </div>
+
+  <div class="contai4">
+    <h2>CS2GO : จุดหมายปลายทางชั้นนำของคุณสำหรับตลาดสกินที่ดีที่สุด</h2>
+    <div class="bigmarket">
+      <div class="market">
+        <div class="marketflex">
+          <div class="mtex">
+            <h3>I'm the buyer</h3>
+            <b>รับมากขึ้นในราคาที่ถูกลง</b>
+          <p>เพลิดเพลินกับราคาที่ต่ำกว่า steam Marketplace ถึง 100%</p>
+        </div>
+        <img src="src\assets\pic\trolley-cart.png" alt="">
+      </div>
+        <button>ไปตลาด</button>
+      </div>
+      <div class="market">
+        <div class="marketflex">
+          <div class="mtex">
+            <h3>I'm the seller</h3>
+            <b>2% commission</b>
+            <p>Enjoy the Lowest Commission in the Market</p>
+          </div>
+          <img src="src\assets\pic\shop.png" alt="">
+        </div>
+          <button>รายการ</button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
     <!-- เนื้อหาแอปของคุณ -->
     <RouterView />
   </div>
